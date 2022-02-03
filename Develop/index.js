a2baed4b6e0d7cac04a4b17848266358e26b442f
omcewan <orlandomcewan231@gmail.com>
2022-02-02 19:46:16 -0500

    added sample qeustions for inquierer

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,34 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "project name",
+    message: "What is the name of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message:
+      "Please describe your project? What is it about? Why did you make? And how was it made?",
+  },
+  {
+    type: "checkbox",
+    name: "languages",
+    message: "What did you build this project with? (Check all that apply)",
+    choices: [
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "ES6",
+      "jQuery",
+      "Bootstrap",
+      "Node",
+    ],
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +38,6 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+const promptUser = () => inquirer.prompt(questions);
+promptUser(questions).then((data) => console.log(data));
